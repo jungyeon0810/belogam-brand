50398c355a7f52614fb291297c39c849a2959b27
jungyeon2999 <jungyeon2999@gmail.com>
2025-05-19 16:58:05 +0900

    0519 작업내역

---
+++ layout/bottom.php
@@ -0,0 +1,48 @@
+<footer id="ft">
+    <div class="d-flex">
+        <div cass="d-flex">
+        <h1><a href=""><img src="/images/icon/logo/logo01.svg" alt="로고"></a></h1>
+
+        <div class="link">
+        <a href="#">개인정보처리방침</a> |
+        <a href="#">이용약관</a> |
+        <a href="#">회사소개</a> |
+        <a href="#">고객지원</a> |
+        <a href="#">입점문의</a>
+       </div>
+
+       </div>
+
+       <div class="d-flex">
+        <div class="cn">
+            <p class="pc_f_menu02"><strong>대표</strong> 홍길동 &nbsp;&nbsp; <strong>사업자번호</strong> 124-94-99017</p>
+            <p><strong>본사</strong> 주소 서울 구로구 공원로 83 (구로동) 4층</p>
+            <p><strong>이메일</strong> Belogam2025@gmail.com</p>
+            <p>COPYRIGHT © BeloGem. ALL RIGHTS RESERVED</p>
+        </div>
+
+        <div class="cs">
+            <p class="customer-center pc_f_menu01">고객센터</p>
+            <p class="phone-number pc_title01">02-000-0000</p>
+            <p class="pc_date_t03">평일 : 09:00 - 18:00</p>
+            <p class="pc_date_t03">주말/공휴일 : 휴무</p>
+        </div>
+
+        <div>
+        <select class="btn">
+                <option>Family Site</option>
+                <option>BeloGem</option>
+                <option>LubiGem</option>
+        </select>
+
+        <div class="sns">
+            <a href="#"><img src="/images/icon/sns/instagram.png" alt="인스타그램"></a>
+            <a href="#"><img src="/images/icon/sns/kakao.png" alt="카카오"></a>
+            <a href="#"><img src="/images/icon/sns/youtube.png" alt="유튜브"></a>
+            <a href="#"><img src="/images/icon/sns/facebook.png" alt="페이스북"></a>
+        </div>
+        
+            </div>
+            </div>
+    </div>
+</footer>
