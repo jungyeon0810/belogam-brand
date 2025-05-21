47ebfdfc4c83d3bb466c1d11747fa3c8c68b851c
jungyeon2999 <jungyeon2999@gmail.com>
2025-05-21 17:28:52 +0900

    0521 패밀리사이트 작업중

--- layout/bottom.php
+++ layout/bottom.php
@@ -1,48 +1,76 @@
+
 <footer id="ft">
-    <div class="d-flex">
-        <div cass="d-flex jcb aic">
-        <h1><a href=""><img src="/images/icon/logo/logo01.svg" alt="로고"></a></h1>
+  
+ <div class="box1 d-flex jcb aie">
+    <h1><img src="/images/icon/logo/logo01.svg" alt=""></h1>
 
-        <div class="link">
-        <a href="#">개인정보처리방침</a> |
-        <a href="#">이용약관</a> |
-        <a href="#">회사소개</a> |
-        <a href="#">고객지원</a> |
-        <a href="#">입점문의</a>
-       </div>
+    <div class="list d-flex aic">
+        <a href=""><strong>개인정보처리방침</strong></a>
+        <img src="/images/img/f_line.png" alt="">
 
-       </div>
+        <a href="">이용약관</a>
+        <img src="/images/img/f_line.png" alt="">
 
-       <div class="d-flex">
-        <div class="cn d-flex">
-            <p class="pc_f_menu02"><strong>대표</strong> 홍길동 &nbsp;&nbsp; <strong>사업자번호</strong> 124-94-99017</p>
-            <p><strong>본사</strong> 주소 서울 구로구 공원로 83 (구로동) 4층</p>
-            <p><strong>이메일</strong> Belogam2025@gmail.com</p>
-            <p>COPYRIGHT © BeloGem. ALL RIGHTS RESERVED</p>
-        </div>
+        <a href="">회사소개</a>
+        <img src="/images/img/f_line.png" alt="">
 
-        <div class="cs d-flex f-d">
-            <p class="customer-center pc_f_menu01">고객센터</p>
-            <p class="phone-number pc_title01">02-000-0000</p>
-            <p class="pc_date_t03">평일 : 09:00 - 18:00</p>
-            <p class="pc_date_t03">주말/공휴일 : 휴무</p>
-        </div>
+        <a href="">고객지원</a>
+        <img src="/images/img/f_line.png" alt="">
 
-        <div class="d-flex f-d">
-        <select class="btns">
-                <option>Family Site</option>
-                <option>BeloGem</option>
-                <option>LubiGem</option>
-        </select>
+        <a href="">입점문의</a>
 
-        <div class="sns">
-            <a href="#"><img src="/images/icon/sns/instagram.png" alt="인스타그램"></a>
-            <a href="#"><img src="/images/icon/sns/kakao.png" alt="카카오"></a>
-            <a href="#"><img src="/images/icon/sns/youtube.png" alt="유튜브"></a>
-            <a href="#"><img src="/images/icon/sns/facebook.png" alt="페이스북"></a>
-        </div>
-        
-            </div>
-            </div>
     </div>
+ </div>
+
+      <div class="under-line d-flex jcb aie">
+    <div class="company d-flex f-d">
+      <div class="box1-1 d-flex">
+        <p class="d-flex pc_f_menu01">대표 <span class="pc_f_menu02">홍길동</span></p>
+        <p class="d-flex pc_f_menu01">사업자번호 <span class="pc_f_menu02">000-00-00000</span></p>
+      </div>
+      <p class="d-flex pc_f_menu01">본사 <span class="pc_f_menu02">서울시 구로구 공원로 83 (구로동) 4층</span></p>
+      <p class="d-flex pc_f_menu01">이메일 <span class="pc_f_menu02">belogam2025@gmail.com</span></p>
+      <div class="box1-2">
+      <p class="pc_f_menu01">COPYRIGHT ⓒ BELOGEM. ALL RIGHTS RESERVED</p>
+      </div>
+    </div>
+
+    <div class="cs-center d-flex f-d">
+      <p class="pc_f_menu01">고객센터</p>
+      <p class="pc_title01">02-000-0000</p>
+      <div class="time d-flex f-d">
+        <p class="pc_date_t03">평일 : 09:00 - 18:00</p>
+        <p class="pc_date_t03">주말/공휴일 : 휴무</p>  
+      </div>      
+    </div>
+
+    <div class="family d-flex f-d">
+    <select name="speed" id="speed_ddd">
+      <option>BeloGem</option>
+      <option>LuvieGem</option>
+    </select>
+
+
+      <div class="sns d-flex jcb">
+          <a href="#"><img src="/images/icon/sns/instagram.png" alt="인스타그램"></a>
+          <a href="#"><img src="/images/icon/sns/kakao.png" alt="카카오"></a>
+          <a href="#"><img src="/images/icon/sns/youtube.png" alt="유튜브"></a>
+          <a href="#"><img src="/images/icon/sns/facebook.png" alt="페이스북"></a>
+      </div>
+      
+          </div>
+  </div>
+
+  <script
+  src="https://code.jquery.com/jquery-2.2.4.min.js"
+  integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+  crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/ui/1.14.1/jquery-ui.js"></script>
+  <link rel="stylesheet" href="https://code.jquery.com/ui/1.14.1/themes/base/jquery-ui.css">
+  <script>
+
+    $(document).ready(function(){
+       $("#speed_ddd").selectmenu();
+    })
+  </script>
 </footer>
